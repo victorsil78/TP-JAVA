e49bc7e8c43471a10ea6faf6179d66b218d08a37
mrosti8 <rockandrost@gmail.com>
2022-06-11 17:43:58 -0300

    Cambios GamePlayer

--- futbolTP1/src/Game/GamePlayer.java
+++ futbolTP1/src/Game/GamePlayer.java
@@ -2,6 +2,8 @@ package Game;
 
 import Club.Club;
 
+import java.util.List;
+
 public class GamePlayer {
     private String name;
     private Club club;
@@ -9,15 +11,17 @@ public class GamePlayer {
 
     //region Constructors
 
-    public GamePlayer(String name, Club club, int wins) {
-        this.name = name;
-        this.club = club;
-        this.wins = wins;
-    }
+    public GamePlayer(String name, Club club) {
+            this.name = name;
+            this.club = club;
+        }
 
-    public GamePlayer() {
-    }
-    //endregion
+    public GamePlayer(List<Club> clubList) {
+                this.name = "PC";
+                this.club = randomClub(clubList);
+            }
+
+            //endregion
     //region Getters
 
     public String getName() {
@@ -61,6 +65,21 @@ public class GamePlayer {
 
     //endregion
     //region Methods
+    public int clubCounter(List<Club> clubList){
+        int total = 0;
+        for (var i : clubList){
+            total ++;
+        }
+        return total;
+    }
+
+    public Club randomClub (List<Club> clubList){
+        int total = clubCounter(clubList);
+        int random = (int)(Math.random()*total);
+        Club club = clubList.get(random);
+        clubList.remove(club);
+        return club;
+    }
     //endregion
 
 }
